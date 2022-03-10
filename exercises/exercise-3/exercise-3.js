227fa0a6cb846297c42037b81a59bc5543ead40c
KatarzynaRuksza <91133235+KatarzynaRuksza@users.noreply.github.com>
2022-03-10 16:42:21 +0000

    Update exercise-3.js

--- exercises/exercise-3/exercise-3.js
+++ exercises/exercise-3/exercise-3.js
@@ -6,3 +6,25 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPrice: 1.0 },
   { itemName: "Hash Brown", quantity: 4, unitPrice: 0.4 },
 ];
+
+
+ 
+let i = 0;
+
+const totalPriceOfEachItems = (quantity, unitPrice) => {
+  let total = 0;
+  total = quantity * unitPrice;
+  return total;
+};
+
+  console.log("QTY     ITEM         TOTAL");
+
+  order.forEach(({ itemName, quantity, unitPrice }) => {
+
+    let total = totalPriceOfEachItems(quantity, unitPrice);
+    i = i + total;
+
+    console.log(`${quantity}     ${itemName}       ${total}`);
+  });
+
+  console.log(`Bill:`, i);
